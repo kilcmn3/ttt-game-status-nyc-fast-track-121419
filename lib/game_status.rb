921ad2f68751e2423b57a3245c38e584d86a642d
DAVID SHIN <kilcmn3@hotmail.com>
2019-12-15 16:42:30 +0000

    Automatically backed up by Learn

--- lib/game_status.rb
+++ lib/game_status.rb
@@ -4,3 +4,16 @@ def position_taken?(board, index)
 end
 
 # Define your WIN_COMBINATIONS constant
+WIN_COMBINATIONS = [
+  [0,1,2], # Top row
+  [3,4,5],  # Middle row
+  [6,8,9], #bottom row
+  #vertical win comb
+  [0,3,6],
+  [1,4,8],
+  [2,5,9],
+  #2 diagonals
+  [0,4,9],
+  [2,4,6]
+  
+]
