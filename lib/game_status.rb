9f5d4658b87018f0781e40e362b9b79006abca0b
DAVID SHIN <kilcmn3@hotmail.com>
2019-12-15 19:32:33 +0000

    Automatically backed up by Learn

--- lib/game_status.rb
+++ lib/game_status.rb
@@ -46,4 +46,18 @@ end
 return false
 end
 
-#{name}
+def full?(board_array)
+
+    WIN_COMBINATIONS.each do |combo_arr|
+      first_index_1 = combo_arr[0]
+      second_index_2 = combo_arr[1]
+      third_index_3 = combo_arr[2]
+
+      the_symbol_at_1 = board_arr[first_index_1] # load the value of the board at win_index_1
+      the_symbol_at_2 = board_arr[second_index_2] # load the value of the board at win_index_2
+      the_symbol_at_3 = board_arr[third_index_3]
+
+      if the_symbol_at_3 || the_symbol_at_2 || the_symbol_at_1
+        == " "
+        return false
+end
